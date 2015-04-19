ead7e253fe50d724c92a13852283f1ca1588147e
alexanderest <alexracer79@hotmail.com>
2015-04-18 20:20:08 -0400

    Altered BookSearch

--- BookService/BookSearch.aspx
+++ BookService/BookSearch.aspx
@@ -5,28 +5,41 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 26px;
+        }
+    </style>
 </head>
-<body>
+<body style="height: 251px">
     <form id="form1" runat="server">
     <div>
     
         <table style="width:100%;">
             <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
+                <td class="auto-style1">
+                    <asp:Label ID="Label1" runat="server" Text="Search For a Book Review: "></asp:Label>
+                </td>
+                <td class="auto-style1">
+                    <asp:DropDownList ID="DropDownList1" runat="server">
+                        <asp:ListItem Text="By Book ID"></asp:ListItem>
+                        <asp:ListItem Text="By Book Name"></asp:ListItem>
+                        <asp:ListItem Text="By reviewer name"></asp:ListItem>
+
+                    </asp:DropDownList>
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style1">
+                    <asp:Button ID="Button1" runat="server" Text="Search" />
+                </td>
             </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-            </tr>
+            
         </table>
+    
+        <asp:GridView ID="GridView1" runat="server" Width="100%">
+        </asp:GridView>
     
     </div>
     </form>
--- BookService/BookSearch.aspx.designer.cs
+++ BookService/BookSearch.aspx.designer.cs
@@ -20,5 +20,50 @@ namespace BookService {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
